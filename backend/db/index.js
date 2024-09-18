3455c2c6a4b66fb959e10d8ff1f182673c75c21e
Rajesh Mali <rajeshmali@cyntexa.com>
2024-09-18 10:37:25 +0530

    first commit

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,7 +2,19 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
 const admin = require('firebase-admin');
-const serviceAccount = require('../course-selling-app-83a6b-firebase-adminsdk-t8xer-9f3aff58e0.json');
+const serviceAccount = {
+  "type": process.env.type,
+  "project_id": process.env.project_id,
+  "private_key_id": process.env.private_key_id,
+  "private_key": process.env.private_key,
+  "client_email": process.env.client_email,
+  "client_id": process.env.client_id,
+  "auth_uri": process.env.auth_uri,
+  "token_uri": process.env.token_uri,
+  "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+  "client_x509_cert_url": process.env.client_x509_cert_url,
+  "universe_domain": process.env.universe_domain
+}
 const multer = require('multer');
 const crypto = require('crypto');
 const path = require('path');
