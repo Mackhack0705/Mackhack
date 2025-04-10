fec314f43085a3382d83e65465a74f84f72849e8
Rajesh Mali <malirajesh0705@gmail.com>
2025-04-10 15:18:48 +0530

    Made the review section responsive as per screen sizes

--- frontend/src/components/ReviewSection.tsx
+++ frontend/src/components/ReviewSection.tsx
@@ -23,7 +23,7 @@ const ReviewCard = ({ children }: { children: any}) => {
     <div
       ref={divRef}
       onMouseMove={handleMouseMove}
-      className='card-spotlight custom-spotlight-card'
+      className='card-spotlight sm:w-1/3 md:w-12/25 lg:w-1/4 xl:w-1/5'
     >
       <div className='flex flex-col justify-center items-center w-full h-full p-4 rounded-lg shadow-lg'>
         <img src={children.userImg} alt="User" className='w-16 h-16 rounded-full mb-4' />
@@ -37,7 +37,7 @@ const ReviewCard = ({ children }: { children: any}) => {
 const ReviewSection = () => {
 
   return (
-    <div className="flex justify-between my-20 px-10">
+    <div className="flex justify-between my-20 px-2 gap-2 flex-wrap md:px-8 md:gap-2 lg:px-4 lg:gap-1 xl:px-10">
       {reviews.map((review) => (
         <ReviewCard key={review.id}>
           {review}
--- frontend/src/components/VideoSection.tsx
+++ frontend/src/components/VideoSection.tsx
@@ -3,76 +3,50 @@ import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useEffect, useRef } from 'react';
 
+const useVideoAutoPlay = (videoRef: React.RefObject<HTMLVideoElement>) => {
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          videoRef.current?.play().catch(error => {
+            console.log('Video play failed:', error);
+          });
+          if (videoRef.current) {
+            videoRef.current.muted = false;
+          }
+        } else {
+          videoRef.current?.pause(); 
+          if (videoRef.current) {
+            videoRef.current.muted = true;
+          } // Pause the video if it leaves the viewport
+        }
+      },
+      { threshold: 0.5 } // Adjust threshold to define visibility percentage
+    );
+
+    const videoElement = videoRef.current;
+    if (videoElement) {
+      observer.observe(videoElement);
+
+      // videoElement.muted = true;
+    }
+
+    return () => {
+      if (videoElement) {
+        observer.unobserve(videoElement);
+      }
+    };
+  }, [videoRef]);
+};
+
 const VideoSection = () => {
   const videoRef1 = useRef<HTMLVideoElement>(null);
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          videoRef1.current?.play();
-        } else {
-          videoRef1.current?.pause(); // Pause the video if it leaves the viewport
-        }
-      },
-      { threshold: 0.5 } // Adjust threshold to define visibility percentage
-    );
-
-    if (videoRef1.current) {
-      observer.observe(videoRef1.current);
-    }
-
-    return () => {
-      if (videoRef1.current) {
-        observer.unobserve(videoRef1.current);
-      }
-    };
-  }, []);
   const videoRef2 = useRef<HTMLVideoElement>(null);
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          videoRef2.current?.play();
-        } else {
-          videoRef2.current?.pause(); // Pause the video if it leaves the viewport
-        }
-      },
-      { threshold: 0.5 } // Adjust threshold to define visibility percentage
-    );
-
-    if (videoRef2.current) {
-      observer.observe(videoRef2.current);
-    }
-
-    return () => {
-      if (videoRef2.current) {
-        observer.unobserve(videoRef2.current);
-      }
-    };
-  }, []);
   const videoRef3 = useRef<HTMLVideoElement>(null);
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          videoRef3.current?.play();
-        } else {
-          videoRef3.current?.pause(); // Pause the video if it leaves the viewport
-        }
-      },
-      { threshold: 0.5 } // Adjust threshold to define visibility percentage
-    );
 
-    if (videoRef3.current) {
-      observer.observe(videoRef3.current);
-    }
-
-    return () => {
-      if (videoRef3.current) {
-        observer.unobserve(videoRef3.current);
-      }
-    };
-  }, []);
+  useVideoAutoPlay(videoRef1);
+  useVideoAutoPlay(videoRef2);
+  useVideoAutoPlay(videoRef3);
 
   gsap.registerPlugin(ScrollTrigger);
 
@@ -98,19 +72,19 @@ const VideoSection = () => {
   return (
     <div id='target-section' className='h-screen mb-40 flex justify-center items-center'>
         <div id='video-section' className='w-full h-full justify-center items-center hidden md:hidden lg:flex'>
-          <video ref={videoRef1} className='w-full h-full object-cover' >
+          <video autoPlay playsInline ref={videoRef1} className='w-full h-full object-cover' >
             <source src="./videos/videoSection1.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
         <div id='video-section' className='w-full h-full flex justify-center items-center md:hidden lg:hidden'>
-          <video ref={videoRef2} className='w-full h-full object-cover'>
+          <video autoPlay playsInline ref={videoRef2} className='w-full h-full object-cover'>
             <source src="./videos/videoSection2.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
         <div id='video-section' className='w-full h-full hidden justify-center items-center md:flex lg:hidden'>
-          <video ref={videoRef3} className='w-full h-full object-cover'>
+          <video autoPlay playsInline ref={videoRef3} className='w-full h-full object-cover'>
             <source src="./videos/videoSection3.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
